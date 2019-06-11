58d29b1addb1d21a7e63ed9459dac460cbb5f90b
Taras Vynar <vynar.taras@gmail.com>
2019-06-11 16:34:17 +0300

    Add project files.

---
+++ Game15.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace game
+{
+    class Game15
+    {
+        int size;
+        int[,] map;
+        private int spaceX;
+        private int spaceY;
+        public int SpaceX { get; }
+        public int SpaceY { get; }
+        static Random rand = new Random();
+        public Game15(int Size = 4)
+        {
+            size = Size;
+            map = new int[size, size];
+        }
+
+        public void start()
+        {
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    map[x, y] = coordsToPosition(x, y) + 1;
+                }
+            }
+            spaceX = size - 1;
+            spaceY = size - 1;
+            map[spaceX, spaceY] = 0;
+        }
+        public int shift(int position)
+        {
+            int x, y, res;
+
+            positionToCoords(position, out x, out y);
+            if (Math.Abs(spaceX - x) + Math.Abs(spaceY - y) != 1) return -1;
+            map[spaceX, spaceY] = map[x, y];//!
+            map[x, y] = 0;
+            spaceX = x;
+            spaceY = y;
+            return position;
+        }
+        public void shiftBack(int position)
+        {
+            if (position != -1 || position != 16)
+            {
+                int x, y;
+                positionToCoords(position, out x, out y);
+                map[spaceX, spaceY] = map[x, y];//!
+                map[x, y] = 0;
+                spaceX = x;
+                spaceY = y;
+            }
+
+
+        }
+        public string shuffler()
+        {
+            //shift(rand.Next(0, size * size));
+            int a = rand.Next(0, 4);
+            int x = spaceX;
+            int y = spaceY;
+            int direction=0;
+            string separator = ",";
+            switch (a)
+            {
+                case 0:
+                    x++;
+                    direction = 1;
+                    break; // ліво
+                case 1:
+                    x--;
+                    direction = 2;
+                    break; //право
+                case 2:
+                    y++;
+                    direction = 3;
+                    break; // верх
+                case 3:
+                    y--;
+                    direction = 4;
+                    break;  //низ
+            }
+            //shift(coordsToPosition(x, y));
+
+            return shift(coordsToPosition(x, y)).ToString() + separator + direction.ToString();
+            //return (coordsToPosition(x, y) + 1).ToString() + ". " + direction;
+
+        }
+        public void solver(int a)
+        {
+            int x = spaceX;
+            int y = spaceY;
+            switch (a)
+            {
+                case 0:
+                    x--;
+                    break; // ліво
+                case 1:
+                    x++;
+                    break; //право
+                case 2:
+                    y--;
+                    break; // вниз
+                case 3:
+                    y++;
+                    break; //вверх
+            }
+            shift(coordsToPosition(x, y));
+        }
+        public bool check_numbers()
+        {
+            if (!(spaceX == size - 1 && spaceX == size - 1)) return false;
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    if (!(x == size - 1 && y == size - 1))
+                        if (map[x, y] != coordsToPosition(x, y) + 1)
+                            return false;
+            return true;
+
+
+        }
+        public int getNumber(int position)
+        {
+            int x, y;
+            positionToCoords(position, out x, out y);
+            if (x < 0 || x >= size) return 0;
+            if (y < 0 || y >= size) return 0;
+            return map[x, y];
+        }
+
+        private int coordsToPosition(int x, int y)
+        {
+            if (x < 0) x = 0;
+            if (x > size - 1) x = size - 1;
+            if (y < 0) y = 0;
+            if (y > size - 1) y = size - 1;
+            return y * size + x;
+        }
+        private void positionToCoords(int position, out int x, out int y)
+        {
+            if (position < 0) position = 0;
+            if (position > size * size - 1) position = (size * size) - 1;
+            x = position % size;
+            y = position / size;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string path = @"1.txt";
+        string path2 = @"2.txt";
+        string path3 = @"3.txt";
+
+        Game15 game;
+        public MainWindow()
+        {
+            InitializeComponent();
+            game = new Game15(4);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int position = Convert.ToInt32(((Button)sender).Tag);
+            game.shift(position);
+            //if (game.check_numbers())
+            //{
+            //    MessageBox.Show("Win", "Win");
+            //    start_game();
+            //}
+            refresh();
+        }
+        private Button button(int position)
+        {
+            switch (position)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button9;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+                default: return null;
+
+
+            }
+        }
+
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            start_game();
+            using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+            {
+
+                for (int i = 0; i < 5; i++)
+                {
+
+                    sw.WriteLine(game.shuffler());
+                }
+                sw.Close();
+            }
+            refresh();
+            using (StreamWriter sw = new StreamWriter(path2, false, System.Text.Encoding.Default))
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        sw.Write(button(i * 4 + j).Content + "|");
+                    }
+                    sw.WriteLine();
+
+                }
+                sw.Close();
+            }
+
+        }
+        private void start_game()
+        {
+            game.start();
+            refresh();
+        }
+        private void refresh()
+        {
+            for (int position = 0; position < 16; position++)
+            {
+                button(position).Content = game.getNumber(position);
+                if (game.getNumber(position) > 0) button(position).Visibility = Visibility.Visible;
+                else button(position).Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            start_game();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            string[] lines = File.ReadAllLines(path);
+
+            IEnumerable<string> revLines = lines.Reverse();
+
+            
+                foreach (string line in revLines)
+                {
+                    string[] divided = line.Split(',');
+                    if (Convert.ToInt32(divided[1]) == 1)
+                    {
+                        game.shift((Convert.ToInt32(divided[0]) - 1));
+                    }
+                    if (Convert.ToInt32(divided[1]) == 2)
+                    {
+                        game.shift((Convert.ToInt32(divided[0]) + 1));
+                    }
+                    if (Convert.ToInt32(divided[1]) == 3)
+                    {
+                        game.shift((Convert.ToInt32(divided[0]) - 4));
+                    }
+                    if (Convert.ToInt32(divided[1]) == 4)
+                    {
+                        game.shift((Convert.ToInt32(divided[0]) + 4));
+                    }
+
+                    refresh();
+                    Thread.Sleep(5000);
+                }
+                if (game.check_numbers())
+                {
+                    MessageBox.Show("Win", "Win");
+                    start_game();
+                    return;
+
+                }
+
+            
+
+        }
+    }
+}
+
